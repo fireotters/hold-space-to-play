0de71fce67dd402439bf166759dc390a4c0f743b
Rubén Bermejo Romero <mejoruben@gmail.com>
2019-08-03 10:26:12 +0200

    Start of doin the code for the one way platforms

---
+++ Assets/Scripts/OneWayPlatforms.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneWayPlatforms : MonoBehaviour
+{
+    [SerializeField] private Collider parentCollider;
+    SpriteRenderer triggerRenderer;
+
+    void Awake()
+    {
+        
+        triggerRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parentCollider.enabled = false;
+        triggerRenderer.enabled = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PlatformTrigger")
+        {
+            parentCollider.enabled = true;
+        }
+    }
+}
