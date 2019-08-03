ef614b75cc09a312d63986f655bedb8295286fb3
Cameron <cam.crosse97@outlook.com>
2019-08-03 21:59:45 +1000

    Main Menu, Basis for Sounds, Fonts
    - Main Menu essentially done
    -- Play button starts game
    -- Options menu allows changing of Music & SFX
    -- Exit button exits game
    
    - Sound Mixer and exposed groups made to control volume of music and sfx
    - Fonts added
    - Readme added, attributions to go here
    - Assorted asset changes / additions

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicManager : MonoBehaviour
+    {
+
+    public AudioMixer mixer;
+    public AudioSource sfxDemo;
+
+    public void ChangeMusic(float sliderValue) {
+        mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("Music", sliderValue);
+    }
+
+    public void ChangeSFX(float sliderValue) {
+        mixer.SetFloat("SFXVolume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("SFX", sliderValue);
+        if (!sfxDemo.isPlaying) {
+            sfxDemo.Play();
+        }
+    }
+}
