2012e64d4dbb2e34f9f3b5d6d484ca7290ee0346
Rubén Bermejo Romero <mejoruben@gmail.com>
2019-08-03 11:44:05 +0200

    more comment stuff

--- Assets/Scripts/OneWayPlatforms.cs
+++ Assets/Scripts/OneWayPlatforms.cs
@@ -12,20 +12,25 @@ public class OneWayPlatforms : MonoBehaviour
         parentCollider = GetComponent<Collider>();
     }
 
-    // Start is called before the first frame update
     void Start()
     {
+        // If the triggers are not present, error out.
         if (topTriggerRenderer == null || bottomTriggerRenderer == null)
         {
             Debug.LogError("No triggers have been specified!");
             Destroy(gameObject);
         }
 
+        // Disables the platform's collider and the trigger sprites.
         parentCollider.enabled = false;
         topTriggerRenderer.enabled = false;
         bottomTriggerRenderer.enabled = false;
     }
 
+    /// <summary>
+    /// Enables or disables the platform's collider.
+    /// </summary>
+    /// <param name="value">Whether true of false, depending if we want it active or not.</param>
     public void SetPlatformCollider(bool value)
     {
         parentCollider.enabled = value;
